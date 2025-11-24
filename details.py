ac79c831247ad2279f2f1e2e15fa2243fd6533de
yashila verma <yashila.25bai11574@vitbhopal.ac.in>
2025-11-24 23:56:36 +0530

    Add files via upload

---
+++ details.py
@@ -0,0 +1,35 @@
+
+
+
+students=[
+    {"name": "Aarav Mehta", "branch": "CSE", "regno": "CSE2025001", "city": "Delhi"},
+    {"name": "Riya Sharma", "branch": "ECE", "regno": "ECE2025002", "city": "Mumbai"},
+    {"name": "Karan Patel", "branch": "ME",  "regno": "ME2025003",  "city": "Ahmedabad"},
+    {"name": "Sneha Gupta", "branch": "CSE", "regno": "CSE2025004", "city": "Lucknow"},
+    {"name": "Rohit Verma", "branch": "IT",  "regno": "IT2025005",  "city": "Bhopal"},
+
+    {"name": "Isha Nair", "branch": "CSE", "regno": "CSE2025006", "city": "Kochi"},
+    {"name": "Manav Singh", "branch": "EEE", "regno": "EEE2025007", "city": "Chandigarh"},
+    {"name": "Tanvi Desai", "branch": "CE",  "regno": "CE2025008",  "city": "Surat"},
+    {"name": "Yash Malhotra", "branch": "CSE", "regno": "CSE2025009", "city": "Jaipur"},
+    {"name": "Ananya Reddy", "branch": "ECE", "regno": "ECE2025010", "city": "Hyderabad"},
+
+    {"name": "Vikram Rao", "branch": "ME", "regno": "ME2025011", "city": "Vishakhapatnam"},
+    {"name": "Pooja Kulkarni", "branch": "IT", "regno": "IT2025012", "city": "Pune"},
+    {"name": "Aman Khan", "branch": "CSE", "regno": "CSE2025013", "city": "Delhi"},
+    {"name": "Harshita Das", "branch": "EEE", "regno": "EEE2025014", "city": "Bhubaneswar"},
+    {"name": "Siddharth Roy", "branch": "CE", "regno": "CE2025015", "city": "Kolkata"},
+
+    {"name": "Nikita Jain", "branch": "CSE", "regno": "CSE2025016", "city": "Indore"},
+    {"name": "Arjun Paul", "branch": "ECE", "regno": "ECE2025017", "city": "Mangalore"},
+    {"name": "Meera Joshi", "branch": "IT", "regno": "IT2025018", "city": "Nagpur"},
+    {"name": "Kabir Saxena", "branch": "ME", "regno": "ME2025019", "city": "Gurgaon"},
+    {"name": "Shreya Iyer", "branch": "CSE", "regno": "CSE2025020", "city": "Chennai"},
+
+    {"name": "Ritik Pandey", "branch": "EEE", "regno": "EEE2025021", "city": "Varanasi"},
+    {"name": "Diya Kapoor", "branch": "CSE", "regno": "CSE2025022", "city": "Agra"},
+    {"name": "Mohit Rawat", "branch": "CE",  "regno": "CE2025023",  "city": "Dehradun"},
+    {"name": "Aditi Bansal", "branch": "ECE", "regno": "ECE2025024", "city": "Noida"},
+    {"name": "Samar Gill", "branch": "IT",  "regno": "IT2025025",  "city": "Amritsar"}
+]
+
